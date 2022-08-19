83a1856328f5df116bbff6db4a007e3e00e7e3fc
Andreas Rozek <A.Rozek@gmx.de>
2022-08-19 06:22:08 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -4,16 +4,16 @@ import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser'
 
 export default {
-  input: './src/aframe-billboard-component.ts',
+  input: './src/aframe-vertical-billboard-component.ts',
   output: [
     {
-      file:     './dist/aframe-billboard-component.js',
+      file:     './dist/aframe-vertical-billboard-component.js',
       format:    'iife', // module is loaded for side-effects only
       noConflict:true,
       sourcemap: true,
       plugins: [terser({ format:{ comments:false, safari10:true } })],
     },{
-      file:     './dist/aframe-billboard-component.esm.js',
+      file:     './dist/aframe-vertical-billboard-component.esm.js',
       format:   'esm',
       sourcemap:true,
     }
